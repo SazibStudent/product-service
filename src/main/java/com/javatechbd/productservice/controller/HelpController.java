9071de4401fdbe31ff14b30a8972189b5bbaaf4b
muhammad mahadi <muhammad.mahadi@egeneration.co>
2022-07-24 09:29:37 +0600

    updated

---
+++ src/main/java/com/javatechbd/productservice/controller/HelpController.java
@@ -0,0 +1,22 @@
+package com.javatechbd.productservice.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/help")
+@AllArgsConstructor
+public class HelpController {
+
+    private Environment environment;
+
+    @GetMapping
+    public String port() {
+        return environment.getProperty("server.port");
+    }
+}
