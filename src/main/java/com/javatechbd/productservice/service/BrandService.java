cb8534bcc04797046b41e2ff5b0770191bb63e00
sazib <sazibkhancse@gmail.com>
2022-10-03 22:41:20 +0600

    Brand and customar Kafka

--- src/main/java/com/javatechbd/productservice/service/BrandService.java
+++ src/main/java/com/javatechbd/productservice/service/BrandService.java
@@ -1,29 +1,45 @@
 package com.javatechbd.productservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.javatechbd.productservice.dto.request.BrandDto;
 
 import com.javatechbd.productservice.dto.response.BrandRest;
 import com.javatechbd.productservice.entity.BrandEntity;
+import com.javatechbd.productservice.entity.ProductEntity;
 import com.javatechbd.productservice.repository.BrandRepository;
+import jdk.security.jarsigner.JarSignerException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class BrandService {
 
     private final BrandRepository brandRepository;
     private final EntityValidationService entityValidationService;
+
+    private final Environment environment;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void createNewBrand(BrandDto brandDto) {
 
         var brandEntity= new BrandEntity();
         BeanUtils.copyProperties(brandDto,brandEntity);
         brandRepository.save(brandEntity);
+        sendBrandToKafka(brandEntity);
     }
 
 
@@ -64,5 +80,34 @@ public class BrandService {
                     return res;
                 }).collect(Collectors.toList());
     }
+    public void sendBrandToKafka(BrandEntity brandEntity)
+    {
+        var brandString = getBrandAsString(brandEntity);
+        ListenableFuture<SendResult<String, String>> future =
+                this.kafkaTemplate.send(environment.getProperty("application.topic.brand-request"), brandString);
+
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Sent BrandEntity: " + brandString + " with offset: " + result.getRecordMetadata().offset());
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send brandEntity : " + brandEntity, ex);
+            }
+        });
+    }
+
+    private  String getBrandAsString(BrandEntity brandEntity){
+            ObjectWriter ow=new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            return ow.writeValueAsString(brandEntity);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
 
 }
--- src/main/java/com/javatechbd/productservice/service/CustomerService.java
+++ src/main/java/com/javatechbd/productservice/service/CustomerService.java
@@ -1,23 +1,36 @@
 package com.javatechbd.productservice.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.javatechbd.productservice.dto.request.CustomerDto;
 import com.javatechbd.productservice.dto.response.CustomerRest;
 import com.javatechbd.productservice.entity.CustomerEntity;
+import com.javatechbd.productservice.entity.ProductEntity;
 import com.javatechbd.productservice.repository.CustomerRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final EntityValidationService entityValidationService;
 
+    private final Environment environment;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void createNewCustomer(CustomerDto customerDto) {
 
@@ -63,5 +76,31 @@ public class CustomerService {
         customerRepository.deleteById(customerEntity.getId());
     }
 
+    public void sendCustomerToKafka(CustomerEntity customerEntity)
+    {
+        var customerString = getCustomerAsString(customerEntity);
+        ListenableFuture<SendResult<String, String>> future =
+                this.kafkaTemplate.send(environment.getProperty("application.topic.customer-request"), customerString);
+
+        future.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Sent CustomerString: " + customerString + " with offset: " + result.getRecordMetadata().offset());
+            }
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Unable to send customerString : " + customerString, ex);
+            }
+        });
+    }
+
+    private String getCustomerAsString(CustomerEntity customerEntity) {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        try {
+            return ow.writeValueAsString(customerEntity);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
